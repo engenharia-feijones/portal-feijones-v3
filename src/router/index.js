04ba385dbc72005f83df4b4776f040db9fdf6441
Ismael Souza <souz4ismael@gmail.com>
2021-07-08 09:16:57 -0300

    added whatsapp link

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import IndexHome from '../views/Home/Index.vue'
-import IndexAbout from '../views/About/Index.vue'
-import IndexService from '../views/Service/Index.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import IndexHome from "../views/Home/Index.vue";
+import IndexAbout from "../views/About/Index.vue";
+import IndexService from "../views/Service/Index.vue";
+import Whatsapp from "../views/Whatsapp.vue";
 
 const routes = [
-    { path: '', name: 'Home', component: IndexHome },
-    { path: '/about', name: 'About', component: IndexAbout },
-    { path: '/services', name: 'Service', component: IndexService }
-]
+  { path: "/", name: "Home", component: IndexHome },
+  { path: "/about", name: "About", component: IndexAbout },
+  { path: "/services", name: "Service", component: IndexService },
+  { path: "/whatsapp", name: "Whatsapp", component: Whatsapp },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Whatsapp.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  created() {
+    const url = `https://api.whatsapp.com/send?phone=${5571999992891}&text=Olá, gostaria de ver o cardapio!`;
+    window.location = url;
+  },
+};
+</script>
